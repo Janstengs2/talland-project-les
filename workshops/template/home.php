3bdebfae6b91354d375f0571a6baec47a57ccc60
Jan Stengs <j.stengs@student.talland.nl>
2025-09-25 13:55:46 +0200

    Nieuwe bestanden

---
+++ workshops/template/home.php
@@ -0,0 +1,15 @@
+<!<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="CSS/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+<?php
+include "includes/nav.php";
+?>
+<h1>home</h1>
+</body>
+</html>
